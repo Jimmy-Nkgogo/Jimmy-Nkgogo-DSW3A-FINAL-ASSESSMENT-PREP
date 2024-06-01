84ba029320ca22a86bdf4209885134747c5e4d59
Jimmy <nkgogojimmy@gmail.com>
2024-06-01 10:03:16 +0200

    added the Student and Student Record classes

---
+++ Tutorial 1/WindowsFormsApp1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Student
+    {
+        public string studentNumber {get;set;}
+        public double assignmentMark {get;set;}
+        public double testMark {get;set;}
+        public double examMark {get;set;}
+        public double finalMark { get; set; }
+    }
+}
---
+++ Tutorial 1/WindowsFormsApp1/StudentRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    internal class StudentRecord
+    {
+        public List<Student> Students { get; set; }
+        public StudentRecord()
+        {
+            Students = new List<Student>();
+        }
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+    }
+}
