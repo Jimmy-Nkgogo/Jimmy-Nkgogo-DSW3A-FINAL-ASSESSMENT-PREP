6760b930fa7591f9680891077626e72c8968b6a3
Jimmy <nkgogojimmy@gmail.com>
2024-06-01 13:28:02 +0200

    Display all data function works perfectly

--- Lab 2/Lab 2/Program.cs
+++ Lab 2/Lab 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab_2
 {
@@ -15,7 +16,11 @@ namespace Lab_2
             {
                 if (option == 1)
                 {
-                    Console.WriteLine("Display all data");
+                    string[] lines = DisplayAllData();
+                    foreach (var item in lines)
+                    {
+                        Console.WriteLine(item);
+                    }
                 }
                 else if (option == 2)
                 {
@@ -45,6 +50,18 @@ namespace Lab_2
             Console.WriteLine("0: Exit...");
             option = Convert.ToInt32(Console.ReadLine());
             return option;
+        }
+        static string[] DisplayAllData()
+        {
+            string path = @"carData.csv";
+            string[] lines = File.ReadAllLines(path);
+            List<string> formattedLines = new List<string>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                formattedLines.Add(lines[i]);
+            }
+            return formattedLines.ToArray();
         }
     }
 }
