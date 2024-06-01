bb3306b429e55e1dd2a7f123652eb4d293063019
Jimmy <nkgogojimmy@gmail.com>
2024-06-01 09:51:21 +0200

    The compute button is complete and all the calculations are correct

--- Tutorial 1/WindowsFormsApp1/Form1.Designer.cs
+++ Tutorial 1/WindowsFormsApp1/Form1.Designer.cs
@@ -66,10 +66,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(225, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(848, 52);
+            this.label1.Size = new System.Drawing.Size(730, 42);
             this.label1.TabIndex = 1;
             this.label1.Text = "University Of Johannesburg Exam Portal";
             // 
@@ -125,6 +125,8 @@
             this.textBoxStudentNumber.Name = "textBoxStudentNumber";
             this.textBoxStudentNumber.Size = new System.Drawing.Size(185, 28);
             this.textBoxStudentNumber.TabIndex = 7;
+            this.textBoxStudentNumber.TextChanged += new System.EventHandler(this.textBoxStudentNumber_TextChanged);
+            this.textBoxStudentNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStudentNumber_KeyPress);
             // 
             // textBoxAssignment
             // 
@@ -162,7 +164,7 @@
             // textBoxDisplay
             // 
             this.textBoxDisplay.Enabled = false;
-            this.textBoxDisplay.Location = new System.Drawing.Point(492, 190);
+            this.textBoxDisplay.Location = new System.Drawing.Point(467, 190);
             this.textBoxDisplay.Multiline = true;
             this.textBoxDisplay.Name = "textBoxDisplay";
             this.textBoxDisplay.Size = new System.Drawing.Size(456, 171);
@@ -171,7 +173,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(489, 171);
+            this.label7.Location = new System.Drawing.Point(464, 171);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(103, 16);
             this.label7.TabIndex = 13;
@@ -180,7 +182,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(657, 171);
+            this.label8.Location = new System.Drawing.Point(632, 171);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(69, 16);
             this.label8.TabIndex = 14;
@@ -189,7 +191,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(845, 171);
+            this.label9.Location = new System.Drawing.Point(820, 171);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(81, 16);
             this.label9.TabIndex = 15;
@@ -203,6 +205,7 @@
             this.buttonCompute.TabIndex = 16;
             this.buttonCompute.Text = "Compute";
             this.buttonCompute.UseVisualStyleBackColor = true;
+            this.buttonCompute.Click += new System.EventHandler(this.buttonCompute_Click);
             // 
             // buttonAdd
             // 
--- Tutorial 1/WindowsFormsApp1/Form1.cs
+++ Tutorial 1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,44 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        /*
+         You are requested to develop a C# application for out institution. The application determine
+a student’s final mark based on the marks obtained from the assignment, test and
+examination. The assignment, test, and exam has the weights of 20%, 30% and 50%
+respectively. The captured user input and the computed final mark is stores in a data structure
+such as an array of objects. The application must also save all students’ data in a “TXT” file
+called exam.txt. The below diagrams illustrates the operations that that must performed 
+         */
+        private string studentNumber;
+        private double assignmentMark;
+        private double testMark;
+        private double examMark;
+        private double finalMark;
+        private void buttonCompute_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBoxStudentNumber.Text) && String.IsNullOrWhiteSpace(textBoxAssignment.Text) && String.IsNullOrWhiteSpace(textBoxTest.Text) && String.IsNullOrWhiteSpace(textBoxExam.Text))
+            {
+                MessageBox.Show("Fill in the missing textbox");
+            }
+            else
+            {
+                studentNumber = textBoxStudentNumber.Text;
+                assignmentMark = Convert.ToDouble(textBoxAssignment.Text);
+                testMark = Convert.ToDouble(textBoxTest.Text);
+                examMark = Convert.ToDouble(textBoxExam.Text);
+                finalMark = Math.Round((assignmentMark * 0.2) + (testMark * 0.3) + (examMark * 0.5),2);
+                textBoxFinal.Text = finalMark.ToString();
+            }
+        }
+
+        private void textBoxStudentNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBoxStudentNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
