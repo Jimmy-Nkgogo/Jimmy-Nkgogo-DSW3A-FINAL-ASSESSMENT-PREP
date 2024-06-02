6b9a101eedb2d526544a1b83aff4b472174732d3
Jimmy <nkgogojimmy@gmail.com>
2024-06-03 01:58:48 +0200

    ran the functions expect for asking for user input

--- Sick Test/Program.cs
+++ Sick Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,16 @@ namespace Sick_Test
     {
         static void Main(string[] args)
         {
+            List<ThirdYearStudent> thirdYearStudents = new List<ThirdYearStudent>();
+
+            ThirdYearStudent thirdYear1 = new ThirdYearStudent();
+            ThirdYearStudent thirdYear2 = new ThirdYearStudent();
+
+            foreach (var student in thirdYearStudents)
+            {
+                student.displayStudent();
+                Console.WriteLine(student.calcFinalMark());
+            }
         }
     }
 }
