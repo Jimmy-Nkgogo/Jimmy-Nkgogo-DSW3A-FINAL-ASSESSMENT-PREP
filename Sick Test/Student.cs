df8af45fc7b79e8a03f0b5667e4da4d478c8d27f
Jimmy <nkgogojimmy@gmail.com>
2024-06-03 01:45:22 +0200

    Done with the abstract classes and methods

---
+++ Sick Test/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Sick_Test
+{
+    abstract class Student
+    {
+        protected int studentNumber;
+        protected string name;
+        protected string surname;
+        public Student()
+        {
+            studentNumber = 0;
+            name = null;
+            surname = null;
+        }
+        public abstract void setStudent(int num, string n, string s);
+
+        public abstract void displayStudent();
+        
+    }
+    class ThirdYearStudent : Student
+    {
+        private string courseCode;
+        private int assignment;
+        private int test;
+
+        public ThirdYearStudent()
+        {
+            
+        }
+        public override void setStudent(int num, string n, string s)
+        {
+            studentNumber = num;
+            name = n;
+            surname = s;
+        }
+        public void setStudent(int num, string n, string s, string CourseCode,int AssignmentMark,int TestMark)
+        {
+            studentNumber = num;
+            name = n;
+            surname = s;
+            courseCode = CourseCode;
+            assignment = AssignmentMark;
+            test = TestMark;
+        }
+        public override void displayStudent()
+        {
+            Console.WriteLine("\n" + studentNumber + " " + name + " " + surname + $"{courseCode} {test} {assignment}");
+        }
+    }
+}
