0911b88d522e85ea4ac66c3e32c3345445049931
Jimmy <nkgogojimmy@gmail.com>
2024-06-03 02:21:15 +0200

    added the business layer and necessary functions

---
+++ FInal Assessment 2023/BusinessLayer/businessClass.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class businessClass
+    {
+        public bool ValidatePassword(string password)
+        {
+            int upperCaseCount = password.Count(char.IsUpper);
+            if (upperCaseCount < 2)
+                return false;
+
+            int lowerCaseCount = password.Count(char.IsLower);
+            if (lowerCaseCount < 2)
+                return false;
+
+            int digitCount = password.Count(char.IsDigit);
+            if (digitCount < 2)
+                return false;
+
+            return true;
+        }
+        public string FormatNumber(string number)
+        {
+            string firstThree = number.Substring(0, 3);
+            string secondThree = number.Substring(3, 3);
+            string last = number.Substring(5);
+
+            string formattedNumber = $"({firstThree}) {secondThree}-{last}";
+            return formattedNumber;
+        }
+        public string generateEmail(string name,string surname)
+        {
+            return $"{name[0]}{surname}@uj.ac.za".ToLower();
+        }
+        public int generateAge(string userId)
+        {
+            string id = userId;
+            string bornYear;
+            int year = int.Parse(id.Substring(0, 2));
+
+            DateTime date = DateTime.Now;
+
+            if (year >= 0 && year <= 9)
+            {
+                if (year < 24)
+                {
+                    bornYear = $"200{year}";
+                }
+                else
+                {
+                    bornYear = $"19{year}";
+                }
+            }
+            else
+            {
+                if (year < 24)
+                {
+                    bornYear = $"20{year}";
+                }
+                else
+                {
+                    bornYear = $"19{year}";
+                }
+            }
+
+            int age = date.Year - int.Parse(bornYear);
+
+            return age;
+        }
+    }
+}
